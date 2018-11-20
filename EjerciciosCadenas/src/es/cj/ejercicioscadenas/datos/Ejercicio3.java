4f6857de3d7c83dcd1dffbf93d0ea1ebbe8def07
Rafa <Rafa@Rafa>
2018-11-20 14:42:53 +0100

    POO

--- EjerciciosCadenas/src/es/cj/ejercicioscadenas/datos/Ejercicio3.java
+++ EjerciciosCadenas/src/es/cj/ejercicioscadenas/datos/Ejercicio3.java
@@ -3,7 +3,7 @@ package es.cj.ejercicioscadenas.datos;
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		String cadena = "Unodosuno";
+		//String cadena = "Unodosuno";
 		
 		// AÑADIR CARACTERES AL FINAL
 	}
---
+++ EjerciciosPOO/src/es/cj/clases/Caja.java
@@ -0,0 +1,69 @@
+package es.cj.clases;
+
+public class Caja {
+
+	// Atributos
+	private double ancho;
+	private double alto;
+	private double largo;
+
+	// Constructores
+	public Caja() {
+
+	}
+
+	public Caja(double ancho, double alto, double largo) {
+		this.ancho = ancho;
+		this.alto = alto;
+		this.largo = largo;
+	}
+
+	// Métodos GET y SET
+	public double getAncho() {
+		return ancho;
+	}
+
+	public void setAncho(double ancho) {
+		this.ancho = ancho;
+	}
+
+	public double getAlto() {
+		return alto;
+	}
+
+	public void setAlto(double alto) {
+		this.alto = alto;
+	}
+
+	public double getLargo() {
+		return largo;
+	}
+
+	public void setLargo(double largo) {
+		this.largo = largo;
+	}
+
+	// Métodos
+	public double obtenerVolumen() {
+		return ancho * alto * largo;
+	}
+	public double obtenerVolumen(double valor) {
+		return ancho * alto * largo * valor;
+	}
+	public void obtenerVolumen(int valor) {
+		
+	}
+	public String toString() {
+		return "Caja [ancho=" + ancho + ", alto=" + alto + ", largo=" + largo + "]";
+	}
+	
+	public boolean equals(Caja aux) {
+		if(this.ancho == aux.ancho && this.largo == aux.largo && this.alto == aux.alto) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+}
